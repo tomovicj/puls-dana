879a5eae100c83bec8511946bdd92c0091473cbc
Tomovic <jovantomovic5@gmail.com>
2025-04-28 00:38:03 +0200

    Backend generated

---
+++ backend/src/main/java/com/jovantomovic/pulsdana/PulsDanaApplication.java
@@ -0,0 +1,13 @@
+package com.jovantomovic.pulsdana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulsDanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulsDanaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jovantomovic/pulsdana/PulsDanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jovantomovic.pulsdana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulsDanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
